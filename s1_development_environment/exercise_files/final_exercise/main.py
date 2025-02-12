6192a2d3572ff13bf6cad683ce32c0e3cd88844c
Lenka Wolter <lenka.wolter@rwth-aachen.de>
2025-02-12 09:15:59 +0100

    new branch

--- s1_development_environment/exercise_files/final_exercise/main.py
+++ s1_development_environment/exercise_files/final_exercise/main.py
@@ -1,7 +1,8 @@
 import torch
 import typer
-from data_solution import corrupt_mnist
+from data import corrupt_mnist
 from model import MyAwesomeModel
+from torch import nn, optim
 
 app = typer.Typer()
 
@@ -12,9 +13,47 @@ def train(lr: float = 1e-3) -> None:
     print("Training day and night")
     print(lr)
 
-    # TODO: Implement training loop here
+    # training loop
     model = MyAwesomeModel()
     train_set, _ = corrupt_mnist()
+
+    train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=32)
+
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+
+    epochs = 5
+    steps = 0
+
+    model.train()
+
+    train_losses = []
+    for _ in range(epochs):
+        running_loss = 0
+        for images, labels in train_dataloader:
+            # Flatten images into a 784 long vector
+            images = images.view(images.shape[0], -1)
+
+            optimizer.zero_grad()
+
+            output = model(images)
+            loss = criterion(output, labels)
+            loss.backward()
+            optimizer.step()
+
+            train_losses.append(loss.item())
+            running_loss += loss.item()
+
+    print("Our model: \n\n", model, "\n")
+    print("The state dict keys: \n\n", model.state_dict().keys())
+
+    model_checkpoint = {
+            "input_size": 784,
+            "output_size": 10,
+            "hidden_layers": [128, 64, 32],
+            "state_dict": model.state_dict(),
+        }
+    torch.save(model_checkpoint, "model_checkpoint.pth")
 
 
 @app.command()
@@ -24,9 +63,25 @@ def evaluate(model_checkpoint: str) -> None:
     print(model_checkpoint)
 
     # TODO: Implement evaluation logic here
-    model = torch.load(model_checkpoint)
+    model = load_checkpoint(model_checkpoint)
     _, test_set = corrupt_mnist()
+    test_dataloader = torch.utils.data.DataLoader(test_set, batch_size=32)
 
+    model.eval()
+    correct, total = 0, 0
+    for img, target in test_dataloader:
+        y_pred = model(img)
+        correct += (y_pred.argmax(dim=1) == target).float().sum().item()
+        total += target.size(0)
+    print(f"Test accuracy: {correct / total * 100}%")
+
+def load_checkpoint(filepath):
+    """Load checkpoint and rebuild the model."""
+    checkpoint = torch.load(filepath)
+    model = MyAwesomeModel(checkpoint["input_size"], checkpoint["output_size"], checkpoint["hidden_layers"])
+    model.load_state_dict(checkpoint["state_dict"])
+
+    return model
 
 if __name__ == "__main__":
     app()
--- s1_development_environment/exercise_files/final_exercise/model.py
+++ s1_development_environment/exercise_files/final_exercise/model.py
@@ -1,9 +1,66 @@
+import torch
 from torch import nn
 
 
 class MyAwesomeModel(nn.Module):
-    """My awesome model."""
+    """
+    Builds a feedforward network with arbitrary hidden layers.
 
-    def __init__(self) -> None:
+    Arguments:
+        input_size: integer, size of the input layer
+        output_size: integer, size of the output layer
+        hidden_layers: list of integers (one for each hidden layer), the sizes of the hidden layers
+    """
+
+    def __init__(self, input_size, output_size, hidden_layers) -> None:
         super().__init__()
-        self.fc1 = nn.Linear(784, 128)
+        
+        input_size = 784
+        hidden_layers = [128, 64, 32]
+        output_size = 10
+
+        # model layers
+        self.fc1 = nn.Linear(input_size, hidden_layers[0])
+        self.fc2 = nn.Linear(hidden_layers[0], hidden_layers[1])
+        self.fc3 = nn.Linear(hidden_layers[1], hidden_layers[2])
+        self.output = nn.Linear(hidden_layers[2], output_size)
+
+        # dropout layer
+        self.dropout = nn.Dropout(p=0.5)
+
+    def forward(self, x):
+        """
+        Forward pass with dropout through the network, returns the output logits.
+        """
+        # flatten input tensor
+        x = x.view(x.shape[0], -1)
+
+        x = self.dropout(torch.relu(self.fc1(x)))
+        x = self.dropout(torch.relu(self.fc2(x)))
+        x = self.dropout(torch.relu(self.fc3(x)))
+        
+        # output
+        x = self.output(x)
+
+        return nn.functional.log_softmax(x, dim=1)
+    
+    def save_checkpoint(state_dict) -> None:
+        """
+        Save the model checkpoint.
+        """
+        model_checkpoint = {
+            "input_size": 784,
+            "output_size": 10,
+            "hidden_layers": [128, 64, 32],
+            "state_dict": state_dict,
+        }
+        torch.save(model_checkpoint, "model_checkpoint.pth")
+    
+if __name__ == "__main__":
+    model = MyAwesomeModel()
+    print(f"Model architecture: {model}")
+    print(f"Number of parameters: {sum(p.numel() for p in model.parameters())}")
+
+    dummy_input = torch.randn(1, 1, 28, 28)
+    output = model(dummy_input)
+    print(f"Output shape: {output.shape}")
